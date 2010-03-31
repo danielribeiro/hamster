93bafcf98a497a74625be99fb72cd2df58228af0
Simon Harris <haruki.zaemon@gmail.com>
2010-03-31 16:17:57 +1100

    The code is now, hopefully, more readable.

--- lib/hamster/immutable.rb
+++ lib/hamster/immutable.rb
@@ -18,7 +18,12 @@ module Hamster
       end
 
       def method_added(name)
-        return unless /^(.*)!$/ === name
+        wrap_method_in_transform(name) if /^(.*)!$/ === name
+      end
+
+      private
+
+      def wrap_method_in_transform(name)
         original = :"___#{$1}_bang___"
         return if method_defined?(original)
         alias_method original, name
